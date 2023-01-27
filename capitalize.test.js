477b0bafbf2d6075e9b2796ab06259e6b3ed8898
mrprotocoll <lekanvgbg@gmail.com>
2023-01-27 18:46:14 +0100

    Add test to capitalize to check for only strings

--- capitalize.test.js
+++ capitalize.test.js
@@ -8,3 +8,12 @@ test("Capitalize first character", () => {
     // test if string is a string
     expect(capitaliseString).toEqual("Damilare")
 })
+
+test("Test for a string", () => {
+    const string = "tony";
+
+    const regex = /\D+/;
+
+    // test if string is a string
+    expect(string).toMatch(regex)
+})
